049e364af77d2c090ed7d6cce8ec392d3bed536c
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2025-10-30 03:18:10 +0000

    Add AppSidebar component and update layout to include it

---
+++ src/app/(protected)/dashboard/app-sidebar.tsx
@@ -0,0 +1,114 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar'
+import { cn } from '@/lib/utils'
+import { Bot, CreditCard, LayoutDashboard, Plus, Presentation } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const items = [
+    {
+        title: 'Dashboard',
+        url: '/dashboard',
+        icon: LayoutDashboard
+    },
+    {
+        title: 'Q&A',
+        url: '/qa',
+        icon: Bot
+    },
+    {
+        title: 'Meetings',
+        url: '/meetings',
+        icon: Presentation
+    },
+    {
+        title: 'Billing',
+        url: '/billing',
+        icon: CreditCard
+    }
+]
+
+const projects = [
+    {
+        title: 'Project Alpha',
+    },
+    {
+        title: 'Project Beta',
+    },
+    {
+        title: 'Project Gamma',
+    }
+]
+
+function AppSidebar() {
+    const pathname = usePathname()
+
+    return (
+        <Sidebar collapsible='icon' variant='floating'>
+            <SidebarHeader>
+                Logo
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>
+                        Application
+                    </SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        {items.map((item) => (
+                            <SidebarMenuItem key={item.title}>
+                                <SidebarMenuButton asChild>
+                                    <Link href={item.url} className={cn({ '!bg-primary !text-white': pathname === item.url })}>
+                                        <item.icon />
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        ))}
+                    </SidebarGroupContent>
+                </SidebarGroup>
+                <SidebarGroup>
+                    <SidebarGroupLabel>
+                        Your Project
+                    </SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {projects.map((project) => (
+                                <SidebarMenuItem key={project.title}>
+                                    <SidebarMenuButton asChild>
+                                        <div>
+                                            <div className={cn(
+                                                'rounded-sm border size-6 flex items-center justify-center text-sm bg-white text-primary',
+                                                {
+                                                    'bg-primary text-white': true
+                                                    // 'bg-primary text-white' : project.id === project.id
+
+                                                }
+                                            )}>
+                                                {project.title.charAt(0)}
+                                            </div>
+                                            {project.title}
+                                        </div>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                            <div className='h-2'></div>
+                            <SidebarMenuItem>
+                                <Link href="/create">
+                                    <Button size={'sm'} variant={"outline"} className='w-fit'>
+                                        <Plus />
+                                        New Project
+                                    </Button>
+                                </Link>
+                            </SidebarMenuItem>
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    )
+}
+
+export default AppSidebar
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarProvider } from "@/components/ui/sidebar"
 import { UserButton } from "@clerk/nextjs"
 import type React from "react"
+import AppSidebar from "./dashboard/app-sidebar"
 
 type Props = {
   children: React.ReactNode
@@ -9,6 +10,7 @@ type Props = {
 const SidebarLayout = ({children}: Props) => {
   return (
     <SidebarProvider>
+      <AppSidebar />
       <main className="w-full m-2">
         <div className="flex items-center border-sidebar-border bg-sidebar border shadow rounded-md py-2 px-4">
           <div className="ml-auto"></div>
