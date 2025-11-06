0b456e9b603e8a5f41cddcf32eda2aa0b42f5882
arnoldart <adoni.091203@gmail.com>
2025-11-06 13:55:51 +0700

    feat: update github lib

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -3,3 +3,20 @@ import { Octokit } from "octokit"
 export const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN
 })
+
+const githubUrl = "https://api.github.com/repos/owner/repo/commits"
+
+type Response = {
+  commitHash: string
+  commitMessage: string
+  commitAuthorName: string
+  commitAuthorAvatar: string
+  commitDate: string
+}
+
+export const getCommitHashes = async (githubUrl: string): Promise<Response[]> => {
+  const {data} = await octokit.rest.repos.listCommits({
+    owner: "arnoldart",
+    repo: "sprintly"
+  })
+}
