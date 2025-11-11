2adcc658424337c296034eb557a787644f92017b
arnoldart <adoni.091203@gmail.com>
2025-11-11 13:46:14 +0700

    feat: enhance GitHub commit fetching with dynamic URL and database integration

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -10,6 +10,7 @@ export default function DashboardPage() {
 
   return (
     <div>
+      {project?.id}
       <div className="flex items-center justify-between flex-wrap gap-y-4">
         <div className="w-fit rounded-md bg-primary px-4 py-3">
           <div className="flex items-center">
--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,10 +1,11 @@
+import { db } from "@/server/db"
 import { Octokit } from "octokit"
 
 export const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN
 })
 
-const githubUrl = "https://api.github.com/repos/owner/repo/commits"
+const githubUrl = "https://github.com/docker/genai-stack"
 
 type Response = {
   commitHash: string
@@ -15,8 +16,62 @@ type Response = {
 }
 
 export const getCommitHashes = async (githubUrl: string): Promise<Response[]> => {
+  const [ owner, repo ] = githubUrl.split('/').slice(-2)
+
+  if(!owner || !repo) {
+    throw new Error("Invalid GitHub URL")
+  }
+
   const {data} = await octokit.rest.repos.listCommits({
-    owner: "arnoldart",
-    repo: "sprintly"
+    owner,
+    repo
   })
+
+  const sortedCommits = data.sort((a, b) => new Date(b.commit.author!.date!).getTime() - new Date(a.commit.author!.date!).getTime()) as any[]
+
+  return sortedCommits.slice(0, 10).map((commit: any) => ({
+    commitHash: commit.sha as string,
+    commitMessage: commit.commit.message ?? "",
+    commitAuthorName: commit.commit.author!.name! ?? "",
+    commitAuthorAvatar: commit.author?.avatar_url ?? "",
+    commitDate: commit.commit.author!.date! ?? ""
+  }))
+}
+
+export const pullCommits = async (projectId: string) => {  
+  const {project, githubUrl} = await FetchProjectGithubURL(projectId)
+  const commmitHashes = await getCommitHashes(githubUrl || "")
+  const unprocessedCommits = await filterUnprocessedCommits(projectId, commmitHashes)
+
+  return unprocessedCommits
+}
+
+async function summarizeCommit(githubUrl: string, commitHash: string) {
+
+}
+
+export const FetchProjectGithubURL = async (projectId: string) => {
+  const project = await db.project.findUnique({
+    where: {id: projectId},
+    select: {
+      repoUrl: true,
+    }
+  })
+
+  return {
+    project, 
+    githubUrl: project?.repoUrl
+  }
+}
+
+export const filterUnprocessedCommits = async (projectId: string, commitHashes: Response[]) => {
+  const processedCommits = await db.commit.findMany({
+    where: {
+      projectId
+    }
+  })  
+
+  const unprocessedCommits = commitHashes.filter((commit) => !processedCommits.some((processedCommit) => processedCommit.commitHash === commit.commitHash))
+  
+  return unprocessedCommits
 }
